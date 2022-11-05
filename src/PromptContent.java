ccbf14c7c46b0b40c2509a8538cec513de0643d6
IATNEHC <98003863+IATNEHC@users.noreply.github.com>
2022-11-05 13:58:53 +0800

    Add files via upload
    component moved out
    gap fixed
    sort function still under development

--- src/PromptContent.java
+++ src/PromptContent.java
@@ -10,59 +10,71 @@ import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class PromptContent extends JFrame {
-    public PromptContent() {
-        //Panel for displaying the student information
-        JPanel upperpanel = new JPanel(new GridLayout(1,1));
-        JTextArea content = new JTextArea(
+    private JTextArea content = new JTextArea(
         "Stduent Name and ID: Chan Kai Ho  (19057769D)\n" +
         "Stduent Name and ID: Sze Kin Sang (19062606D)\n" +
         String.valueOf(new Date())+ "\n\n");
+
+    private JTable bookrecord = new JTable();
+    private DefaultTableModel model = new DefaultTableModel() {
+        public boolean isCellEditable(int row, int column) {
+           //all cells should return false to protect the cells from direct editing
+           return false;
+        }
+    };
+    private Object column[] = { "ISBN", "Title", "Available"};
+    final Object row[] = new Object[3];
+    private JScrollPane scrollpane = new JScrollPane(bookrecord);
+
+    private JTextField isbn = new JTextField(10);
+    private JTextField title = new JTextField(10);
+
+    private JButton add = new JButton("Add");
+    private JButton edit = new JButton("Edit");
+    private JButton save = new JButton("Save");
+    final Object savedindex[] = new Object[1];
+    private JButton delete = new JButton("Delete");
+    private JButton search = new JButton("Search");
+    private JButton more = new JButton("More>>");
+
+    private JButton load = new JButton("Load Test Data");
+    private JButton displayall = new JButton("Display All");
+    private JButton displayall_isbn = new JButton("Display All by ISBN");
+    private JButton displayall_title = new JButton("Display All by Title");
+    private JButton exit = new JButton("Exit");
+
+    public PromptContent() {
+        //Panel for displaying the student information
+        JPanel upperpanel = new JPanel(new GridLayout(1,1)); 
         content.setEditable(false);
         upperpanel.add(content);
         
         //Panel for displaying the JScrollPane
         JPanel middlepanel = new JPanel();
-        JTable bookrecord = new JTable();
-        DefaultTableModel model = new DefaultTableModel() {
-            public boolean isCellEditable(int row, int column) {
-               //all cells should return false to protect the cells from direct editing
-               return false;
-            }
-        };
-        Object column[] = { "ISBN", "Title", "Available"};
-        final Object row[] = new Object[3];
         model.setColumnIdentifiers(column);
         bookrecord.setModel(model);
 
-        JScrollPane scrollpane = new JScrollPane(bookrecord);
         scrollpane.setPreferredSize(new Dimension(680, 270));
         middlepanel.add(scrollpane);
         
         //Panel for displaying the buttons
         JPanel bottompanel = new JPanel(new GridLayout(3,1));
         //Three rows with different content
-        //Input Field
+        //Input Field panel
         JPanel inputfield = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JTextField isbn = new JTextField(10);
-        JTextField title = new JTextField(10);
         inputfield.add(new JLabel("ISBN: "));
         inputfield.add(isbn);
         inputfield.add(new JLabel("Title: "));
         inputfield.add(title);
         bottompanel.add(inputfield);
-        //Button Set 1
+        //Button Set 1 panel
         JPanel buttonset1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JButton add = new JButton("Add");
-        JButton edit = new JButton("Edit");
-        JButton save = new JButton("Save");
         //Disable save button
         save.setEnabled(false);
-        final Object savedindex[] = new Object[1];
-        JButton delete = new JButton("Delete");
-        JButton search = new JButton("Search");
-        JButton more = new JButton("More>>");
         buttonset1.add(add);
         buttonset1.add(edit);
         buttonset1.add(save);
@@ -70,13 +82,8 @@ public class PromptContent extends JFrame {
         buttonset1.add(search);
         buttonset1.add(more);
         bottompanel.add(buttonset1);
-        //Button Set 2
+        //Button Set 2 panel
         JPanel buttonset2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JButton load = new JButton("Load Test Data");
-        JButton displayall = new JButton("Display All");
-        JButton displayall_isbn = new JButton("Display All by ISBN");
-        JButton displayall_title = new JButton("Display All by Title");
-        JButton exit = new JButton("Exit");
         buttonset2.add(load);
         buttonset2.add(displayall);
         buttonset2.add(displayall_isbn);
@@ -219,7 +226,7 @@ public class PromptContent extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 //check if the isbn or title is empty
                 if(isbn.getText().equals("") || title.getText().equals("")) {
-                    JOptionPane.showMessageDialog(null, "ISBN and Title required.");
+                    JOptionPane.showMessageDialog(null, "Error: ISBN and Title required.");
                 }
                 else {
                     boolean repeat = false;
@@ -230,7 +237,7 @@ public class PromptContent extends JFrame {
                         }
                         else if(bookrecord.getValueAt(i,0).toString().equals(isbn.getText())) {
                             repeat = true;
-                            JOptionPane.showMessageDialog(null, "ISBN already exists.");
+                            JOptionPane.showMessageDialog(null, "Error: ISBN already exists.");
                         }
                     }
                     if(!repeat) {
@@ -304,6 +311,12 @@ public class PromptContent extends JFrame {
                     sorter.setRowFilter(filter);
                     bookrecord.setRowSorter(sorter);
                 }
+            }
+        });
+
+        displayall_isbn.addActionListener(new ActionListener() { 
+            public void actionPerformed(ActionEvent e) {
+                
             }
         });
         
