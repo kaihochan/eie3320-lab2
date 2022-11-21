0e124de3ecfa9df3578f9748112e83af5ac141ea
Kai Ho CHAN <kaihojohnchan@gmail.com>
2022-11-21 23:21:20 +0800

    Main method of prompt created.

---
+++ src/RunPrompt.java
@@ -0,0 +1,15 @@
+
+/**
+ * @title   EIE3320 Lab 2: Library Admin System
+ * @author  CHAN Kai Ho 19057769D
+ * @author  SZE Kin Sang 19062606D
+ * @date    3 Nov 2022
+ */
+
+public class RunPrompt
+{
+    public static void main(String[] args) {
+    	MainPrompt mainFrame = new MainPrompt();
+    	mainFrame.setVisible(true);
+    }
+}
